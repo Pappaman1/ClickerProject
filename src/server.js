6e60b35cb0362e2c488c1fe868ea9af036f28606
Pappaman1 <zmw7710@g.rit.edu>
2018-02-26 03:58:18 -0500

    'project'

---
+++ src/server.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const path = require('path');
+//MVC server to host our files
+const express = require('express');
+const socketio = require('socket.io');
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = express();
+
+app.use('/assets', express.static(path.resolve(`${__dirname}/../client/images/`)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../client/client.html`));
+});
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
+server.listen(port, (err) => {
+     if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+    
+});
+
+let serverTotalClicks = 0;
+
+const onJoined = (sock) => {
+  const socket = sock;
+  socket.on('join', () => {
+   console.log("user joined");
+
+    socket.join('room1');
+  });
+};
+
+const dollarClicked = (sock) => {
+    const socket = sock;
+    socket.on('dollarClicked',(data) => {
+        serverTotalClicks += data; 
+        io.sockets.in('room1').emit('updatedTotalClicks',serverTotalClicks);
+        
+    });  
+};
+
+const buyServerUpgrade = (sock) => {
+    const socket = sock;
+    socket.on('buyServerUpgrade',(data) => {
+        serverTotalClicks -= data; 
+        io.sockets.in('room1').emit('updatedTotalClicks',serverTotalClicks);
+        
+    });
+    
+    
+};
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+  onJoined(socket);
+  dollarClicked(socket);
+});
