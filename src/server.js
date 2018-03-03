593349b776a6a166f24bdd432092f5db70544398
Pappaman1 <zmw7710@g.rit.edu>
2018-03-03 17:31:11 -0500

    Project Submission

--- src/server.js
+++ src/server.js
@@ -9,13 +9,15 @@ const app = express();
 
 app.use('/assets', express.static(path.resolve(`${__dirname}/../client/images/`)));
 
+app.use('/css', express.static(path.resolve(`${__dirname}/../client/css/`)));
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../client/client.html`));
 });
 
 const server = http.createServer(app);
 const io = socketio(server);
-
+const users = io.sockets.sockets;
 
 server.listen(port, (err) => {
   if (err) {
@@ -25,13 +27,52 @@ server.listen(port, (err) => {
 });
 
 let serverTotalClicks = 0;
+let currentClickValue = 0;
 
 const onJoined = (sock) => {
   const socket = sock;
-  socket.on('join', () => {
-    console.log('user joined');
+  socket.on('join', (data) => {
+    console.log(data.name);
+    // message back to new user
+    const joinMsg = {
+      name: 'server',
+      msg: `There are ${Object.keys(users).length} users online`,
+    };
+
+    socket.name = data.name;
+    socket.emit('msg', joinMsg);
+
+    users[socket.name] = socket.name;
 
     socket.join('room1');
+    const response = {
+      name: 'server',
+      msg: `${data.name} has joined the room.`,
+    };
+    socket.broadcast.to('room1').emit('msg', response);
+
+    console.log(`${data.name} joined`);
+    socket.emit('msg', { name: 'server', msg: 'You joined the room' });
+  });
+};
+
+const onMsg = (sock) => {
+  const socket = sock;
+  socket.on('msgToServer', (data) => {
+    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg });
+  });
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+  socket.on('disconnect', (data) => {
+    console.dir(data);
+
+    const message = `${socket.name} has left the room.`;
+    socket.broadcast.to('room1').emit('msg', { name: 'server', msg: message });
+    socket.leave('room1');
+
+    delete users[socket.name];
   });
 };
 
@@ -43,16 +84,40 @@ const dollarClicked = (sock) => {
   });
 };
 
-const buyServerUpgrade = (sock) => {
+const subtractServerMoney = (sock) => {
   const socket = sock;
-  socket.on('buyServerUpgrade', (data) => {
+  socket.on('subtractServerMoney', (data) => {
     serverTotalClicks -= data;
     io.sockets.in('room1').emit('updatedTotalClicks', serverTotalClicks);
   });
 };
 
+const upgradeServer = (sock) => {
+  const socket = sock;
+  socket.on('upgradeServer', (data) => {
+    let multiplied = data;
+    multiplied *= 2;
+    currentClickValue = multiplied;
+    io.sockets.in('room1').emit('updatedClickValue', currentClickValue);
+    io.sockets.in('room1').emit('boughtServerUpgradeMsg', { name: socket.name });
+  });
+};
+
+const halfPlayerMoney = (sock) => {
+    const socket = sock;
+    socket.on('halfPlayerMoney', (data) => {
+        let halved = data;
+        halved /= 2;
+        socket.broadcast.to('room1').emit('moneyHalved', halved);
+    });
+};
+
 io.sockets.on('connection', (socket) => {
   onJoined(socket);
   dollarClicked(socket);
-  buyServerUpgrade(socket);
+  subtractServerMoney(socket);
+  upgradeServer(socket);
+  onMsg(socket);
+  onDisconnect(socket);
+  halfPlayerMoney(socket);
 });
